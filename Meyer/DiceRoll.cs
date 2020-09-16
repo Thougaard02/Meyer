a7f902061a371002e603516b6faf4dc902d1996d
Rasmus Thougaard <rasmusvinperlen@outlook.dk>
2020-09-16 15:53:47 +0200

    Doing meyer OOP instead running the program in main

---
+++ Meyer/DiceRoll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meyer
+{
+    class DiceRoll
+    {
+
+        public void DiceGenerator()
+        {
+            Random rd = new Random();
+
+            while (true)
+            {
+                int firstDice = rd.Next(1, 7);
+                int secondDice = rd.Next(1, 6);
+
+                Console.WriteLine(firstDice);
+                Console.WriteLine(secondDice);
+
+            Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Meyer/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meyer
+{
+
+
+    class Player
+    {
+        public string[] playerNames;
+        public void Players()
+        {
+            //Create players
+            Console.WriteLine("Enter the numbers of players 2+ ");
+
+            int amount = Int32.Parse(Console.ReadLine());
+
+            if (amount <= 1)
+            {
+                Console.WriteLine("Minimum two players!");
+            }
+            else
+            {
+                playerNames = new string[amount];
+                for (int i = 0; i < playerNames.Length; i++)
+                {
+                    Console.WriteLine("Insert player " + (i + 1) + " name");
+                    playerNames[i] = Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+
+        //public void PlayerTurn()
+        //{
+        //}
+        public void PLayerChoice()
+        {
+            for (int i = 0; i < playerNames.Length; i++)
+            {
+                if (i == +i)
+                {
+                    Console.WriteLine(playerNames[i] + " What are you going to do");
+                }
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
